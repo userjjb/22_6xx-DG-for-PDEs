db58b923bbb49c58206f190a40fe00f6c995b5e3
Josh Bevan <userjjb@gmail.com>
2015-04-05 15:10:26 -0400

    Final

--- Module 3A/RungePhenom.m
+++ Module 3A/RungePhenom.m
@@ -8,12 +8,14 @@ Lag= @(x,nv) prod(bsxfun(@rdivide,bsxfun(@minus,x,nn(nv,:,:)),bsxfun(@minus,nd(n
 
 interp_f=@(x) f(nd')*Lag(x,1:N);
 xx=0:.001:2;
-plot(xx,interp_f(xx),'b')
+xx2=0:.1:2;
+%plot(xx,interp_f(xx),'b')
+plot(xx2,f(xx2),'o')
 hold on
-plot(xx,f(xx),':k')
 
-%[Qx, Qw]=  GLquad(N);
-% nn2=elim(Qx(1:N)',Qx(1:N)',[1 3 2]);
-% Lag2= @(x,nv) prod(bsxfun(@rdivide,bsxfun(@minus,x,nn2(nv,:,:)),bsxfun(@minus,Qx(nv),nn2(nv,:,:))),3);
-% interp_f2=@(x) f(Qx')*Lag2(x,1:N);
-% plot(xx,interp_f2(xx),'r')
+[Qx, Qw]=  GLquad(N);
+Qx=Qx+1;
+nn2=elim(Qx(1:N)',Qx(1:N)',[1 3 2]);
+Lag2= @(x,nv) prod(bsxfun(@rdivide,bsxfun(@minus,x,nn2(nv,:,:)),bsxfun(@minus,Qx(nv),nn2(nv,:,:))),3);
+interp_f2=@(x) f(Qx')*Lag2(x,1:N);
+plot(xx,interp_f2(xx),'r')
